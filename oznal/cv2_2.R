4c3b99268af76e2674317585dab1d2be0b06be62
pasavec008 <m.adamovic999@gmail.com>
2023-03-03 17:39:24 +0100

    oznal 2_2

--- oznal/cv2_2.R
+++ oznal/cv2_2.R
@@ -9,3 +9,31 @@ m[m > 5]
 m
 m[c(TRUE, FALSE, FALSE, FALSE, TRUE), 2]
 
+
+k <- data.frame(alpha = 1:7, beta = 2:8, gamma = 3:9)
+k
+k$alpha <- NULL
+k
+
+kk <- data.frame(x = 20:30, y = 30:40, z = 40:50, m = 10:20)
+kk[c('x', 'z')] + 1
+
+# to exclue one column
+kk[setdiff(names(kk), 'z')]
+
+t <- c('a', 'a', 'b', 'c')
+lookupTable <- c(a = 'alpha', c = NA)
+lookupTable[t]
+
+
+x <- sample(1:5) ; x
+
+9:1
+mtcars
+mtcars[11:1]
+mtcars[2:5, 11:1]
+
+#random sample from mtcars, but first one and the last one is the same
+mtcars[c(1, sample(1:(nrow(mtcars)), 5), nrow(mtcars)),]
+
+mtcars[order(row.names(mtcars)),]
