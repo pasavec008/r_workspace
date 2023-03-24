b4b752e866ce0cecfb48e376a0386ae467b4afe6
pasavec008 <m.adamovic999@gmail.com>
2023-03-24 16:37:57 +0100

    update 5 1

--- oznal/cv5_1.R
+++ oznal/cv5_1.R
@@ -14,6 +14,7 @@ ames <- ames %>% mutate(Sale_Price = log10(Sale_Price))
 # stratified sampling, which is more complex than random sampling
 set.seed(502)
 ames_split <- initial_split(ames, prop = 0.8, strata = Sale_Price)
+ames_split
 ames_train <- training(ames_split)
 ames_test <- testing(ames_split)
 
@@ -34,9 +35,33 @@ View(df)
 pairs(df_grouped)
 
 # task 4
-
 library(rsample)
 folds <- vfold_cv(df_grouped, v = 5)
-print(folds)
-training_data <- training(folds)
-testing_data <- testing(folds)
+
+fold_1 <- folds$splits[[1]]
+fold_1
+train <- analysis(fold_1)
+test <- assessment(fold_1)
+
+# task 6
+model_train <- lm(Sale_Price~SF_total, data = train)
+plot(model_train)
+qqnorm(train$Sale_Price)
+
+# task 7
+#train
+coef(model_train)
+#test
+model_test <- lm(Sale_Price~SF_total, data = test)
+coef(model_test)
+
+# task 8
+#' We are predicting Sale_Price. Intercept means constant that is always
+#' there and SF_total coeficient means that for every increase of 1 in SF_total,
+#' the Sale_Price is increased by value of this coeficient, which in our case
+#' is about 51
+
+# task 9
+summary(model_train)
+summary(model_test)
+# we can use p value to support our conclusions
