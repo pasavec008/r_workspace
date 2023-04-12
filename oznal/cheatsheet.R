0b1d5a21ce7002438b89e00589ef08ea06491d80
Oliver Kubička <45532792+kubickaoliver@users.noreply.github.com>
2023-04-12 15:39:25 +0000

    Add cheatsheet

--- oznal/cheatsheet.R
+++ oznal/cheatsheet.R
@@ -16,8 +16,8 @@ library(MASS)
 library(ROCit)
 
 ### Load data
-data <- read.csv('data/Default.csv')
-
+data <- read.csv('/Users/oliverkubicka/Desktop/FIIT/OZNAL/data/Default.csv')
+View(data)
 ### Data cleaning
 cleaned_data <- data %>%
   pivot_wider(names_from = Area_SF, values_from = Value_SF) #opposite for pivot_longer
@@ -44,29 +44,68 @@ ggplot(modeldata::crickets, aes(x = temp, y = rate, color = species)) +
 # Show more graphs on one screen
 par(mfrow=c(1,2))
 # Basic analysis of data on plots
-hist(data$balance)
+hist(data$balance, breaks = 50)
 qqnorm(data$balance, main = 'Balance - normal Q-Q')
 qqline(data$balance)
-hist(data[data$default == 'Yes',]$balance, breaks = 50, xlab = 'balance', main = 'Defaulters balance')
+hist(data[data$default == 1,]$balance, breaks = 50, xlab = 'balance', main = 'Defaulters balance')
 # Distribution of variable on two classes on one plot
 ggplot(data, mapping = aes(balance, fill = default)) +
   geom_histogram(alpha=0.6)
 ggplot(data, mapping = aes(default, balance, fill = default)) +
   geom_point(alpha=0.6)
-cor(data)
+
+data %>% mutate(default = as.integer(default), student = as.integer(student)) %>%
+  cor()
 
 ### Models
 # Split data to train set and test set
-data_split <- initial_split(data, 0.8, strata = T)
+data_split <- initial_split(crickets, 0.8, strata = 1)
 data_train <- training(data_split)
 data_test <- testing(data_split)
 # Ordinary Linear Regression (OLR)
-model <- lm(rate ~ temp + species, cric)
+pairs(crickets[crickets$species == 'O. niveus',])
+model <- lm(rate ~ temp + species, data_train)
 summary(model)
+
+# polynomial fit of 5th-degree
+olr_model <- lm(wage_eur ~ poly(skill_dribbling, 5) + poly(skill_curve, 5), data = players_data)
+
 # predict data with model (data must have same structure of predicates [best to use train/test sets])
-predict(model, kk)
+data_test[1:7,] 
+predict(model, data_test)
 # plot model
 # residuals vs fittes -> fitted values are predicted by model (not reality), residual is mistake
 autoplot(model)
 # calculate residual sum of square (measuring size of mistake by residuals^2, direction is not important)
 RSM <- sum(model$residuals^2)
+RSM
+
+# classification
+model <- glm(default ~ balance, data, family = 'binomial')
+# In classification we need to use type = response
+predict(model,
+        newdata = tibble(balance = c(1000,1300,1700,2000,3000)),
+        type = 'response'
+)
+
+# we are deciding what class to choose on cut-off value (0.7)
+data$predicted_default <- ifelse(model$fitted.values > 0.7, 1, 0)
+confusionMatrix(as_factor(data$predicted_default), as_factor(data$default), positive = '1')$table
+
+# LDA
+model <- lda(default ~ balance, data)
+lda_predict <- predict(model)
+lda_predict$class
+
+lda_matrix <- confusionMatrix(lda_predict$class, data$default)
+lda_matrix$table
+
+# Comparison of ROCs between logistic regression and LDA (higher area under curve wins)
+roc <- rocit(class = model_logistic$y,
+             score = model_logistic$fitted.values)
+
+roc2 <- rocit(class = lda_predict$class,
+              score = as.integer(data$default))
+
+summary(roc)
+summary(roc2)
